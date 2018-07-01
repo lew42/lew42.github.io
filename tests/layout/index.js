d4f831b0861e12dba137b4bfae1d3b0b7f8e19b5
Michael Lewis <mike@lew42.com>
2018-07-01 10:11:50 -0500

    c

--- tests/layout/index.js
+++ tests/layout/index.js
@@ -1,9 +1,49 @@
 import simple, { el, div, p, h1, h2, h3, icon, test, assert, write } from "/simple.js";
 simple.stylesheet("/tests/layout/styles.css");
 
+test("test5", t => {
+	t.view.content.css("background-color", "#555");
+	div(".grid3", grid => {
+		div(".left").filler("1-3s");
+		div(".right").filler("1-3s");
+	})
+});
+
+test("test4", t => {
+	t.view.content.css("background-color", "#555");
+	div(".grid-layout", d => {
+		for (var i = 0; i < 50; i++){
+			div(".grid-item.grid-item-" + i, "item" + i).filler("1-4s");
+		}
+	});
+});
+
+test("test3", t => {
+	t.view.content.css("background-color", "#555");
+	div(".grid2", d => {
+		for (var i = 0; i < 10; i++){
+			div(".grid-item", item => {
+				div(".card").filler("1-7s").click(d => d.remove());
+			});
+		}
+	})
+});
+
+test("test2", t => {
+	div(".section1.bg1.cards1", section => {
+		div(".r.grid.three.pad-c.pad1-c.bg-shade1", inner => {
+			for (var i = 0; i < 10; i++){
+				div(".grid-item", item => {
+					div(".card").filler("1-3s").click(d => d.remove());
+				});
+			}
+		})
+	});
+});
+
 test("test1", t=> {
 	div(".section1.bg1", d => {
-		div(".r.grid.two.centered", { left(){
+		div(".r.grid.two", { left(){
 			this.addClass("box1")
 			h1().filler("1s");
 			p().filler("2-4s");
